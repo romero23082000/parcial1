8d291b163ee06a378b35829cde19ccfabf830766
devjunior2titanq <devjunior2titanq@gmail.com>
2022-09-03 16:52:48 -0500

    Nueva constante

--- index.js
+++ index.js
@@ -4,5 +4,6 @@ const mongoose = require('mongoose');
 const app = express();
 app.set('port',  3000)
 
+const nuevaVariable = "prueba"
 app.listen(app.get('port'), ()=>{console.log('listening on port', app.get('port'))})
 console.log("Mensaje de clase hola profe :)")
