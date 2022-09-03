6e3d01f55a4743bd99848bfda7ce8ea82ab822bb
romero23082000 <andreshito435@gmail.com>
2022-09-02 19:25:51 -0500

    Delete mongo

--- index.js
+++ index.js
@@ -5,11 +5,4 @@ const app = express();
 app.set('port',  3000)
 
 
-const user = "andreshito435"
-const password = "andres123"
-const DB_NAME = "Electiva_1"
-const uri = `mongodb+srv://${user}:${password}@cluster0.qiaqzpq.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`
-mongoose.connect(uri).then(()=>console.log("Connect"))
-.catch(e => console.log(e))
-
 app.listen(app.get('port'), ()=>{console.log('listening on port', app.get('port'))})
