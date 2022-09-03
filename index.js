d243fdaf4e168c1df3d19e05cd6a4fbeeef1b493
devjunior2titanq <devjunior2titanq@gmail.com>
2022-09-03 18:11:02 -0500

    Prueba rebase

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const app = express();
 app.set('port',  3000)
 
+const pruebaRebase= "rebase :)"
 const nuevaVariable = "prueba"
 app.listen(app.get('port'), ()=>{console.log('listening on port', app.get('port'))})
 console.log("Mensaje de clase hola profe :)")
