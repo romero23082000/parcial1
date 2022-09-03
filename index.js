d2413a9d632660990eac20c5be9c3d04be47acd2
romero23082000 <andreshito435@gmail.com>
2022-09-02 19:30:06 -0500

    Mensaje

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ const mongoose = require('mongoose');
 const app = express();
 app.set('port',  3000)
 
-
 app.listen(app.get('port'), ()=>{console.log('listening on port', app.get('port'))})
+console.log("Mensaje de clase hola profe :)")
